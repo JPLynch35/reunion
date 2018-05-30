844bfaced1c0c87c70ff7a3d3dea4ff21d227949
JP Lynch <JPLynch35@gmail.com>
2018-05-30 09:31:21 -0600

    added initialize and add participants functionality

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,13 @@
 class Activity
+  attr_reader :name,
+              :participants
+  def initialize(name, participants={})
+    @name = name
+    @participants = participants
+  end
+
+  def add_participants(added_participants)
+    @participants = @participants.merge(added_participants)
+  end
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,40 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def test_it_exists
+    activity = Activity.new('bowling')
+    assert_instance_of Activity, activity
+  end
+
+    def test_activity_has_a_name
+      activity = Activity.new('bowling')
+      assert_equal 'bowling', activity.name
+    end
+
+    def test_activity_can_have_participants
+      participants = {Tom: 5.00,
+                      Sally: 7.00,
+                      Josh: 8.92,
+                      Zach: 3.67}
+      activity = Activity.new('bowling', participants)
+      assert_equal participants, activity.participants
+    end
+
+    def test_can_add_participants_to_an_activity
+      participants = {Tom: 5.00,
+                      Sally: 7.00,
+                      Josh: 8.92,
+                      Zach: 3.67}
+      activity = Activity.new('bowling', participants)
+      participants_to_add = {Jill: 4.32,
+                          Aaron: 1.99}
+      activity.add_participants(participants_to_add)
+      expected = {Tom: 5.00,
+                  Sally: 7.00,
+                  Josh: 8.92,
+                  Zach: 3.67,
+                  Jill: 4.32,
+                  Aaron: 1.99}
+      assert_equal expected, activity.participants
+    end
 end
